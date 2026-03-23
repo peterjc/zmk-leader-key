131216b1f2dea06015d8ef148b93ff33cbf1e84a
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-03-23 00:55:32 +0000

    Was missing &kp def

--- src/behaviors/behavior_leader_key.c
+++ src/behaviors/behavior_leader_key.c
@@ -22,6 +22,8 @@
 #include <zmk/hid.h>
 #include <zmk/matrix.h>
 #include <zmk/keymap.h>
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
 
 #include <zmk-leader-key/keys.h>
 #include <zmk-leader-key/virtual_key_position.h>
